4432314739e4254f23d8b284a557887bc6a31383
toluwase <toluwasethomas1@gmail.com>
2022-02-13 03:40:33 +0100

    GetRandomUser

--- main.go
+++ main.go
@@ -7,6 +7,27 @@ import (
 	"time"
 )
 
+type RandomUser struct {
+Results []UserResult
+}
+
+type UserResult struct {
+	Name UserName `json:"username"`
+	Email string `json:"email"`
+	Picture UserPicture `json:"picture"`
+}
+type UserName struct {
+	Title string
+	First string
+	Last string
+}
+type UserPicture struct {
+	Largest string
+	Medium string
+	Thumbnail string
+}
+
+
 var client  *http.Client
 type CatFact struct {
 	Fact string `json:"fact"`
@@ -22,12 +43,30 @@ func GetCatFact() {
 	} else {
 		fmt.Printf("A interesting cat fact: %s\n", catFact.Fact)
 	}
+}
 
+func GetRandomUser() {
+	url:= "https://randomuser.me/api/?inc=name,email,picture"
+	var randomuser RandomUser
+	err := GetJson(url, &randomuser)
+	if err != nil {
+		fmt.Printf("error getting json: %s\n", err.Error())
+		return
+	} else {
+		fmt.Printf("User: %s %s %s\nEmail: %s\nThumbnail: %s\n",
+			randomuser.Results[0].Name.Title,
+			randomuser.Results[0].Name.First,
+			randomuser.Results[0].Name.Last,
+			randomuser.Results[0].Email,
+			randomuser.Results[0].Picture.Thumbnail,
+		)
+	}
 }
 
 func main() {
 	client = &http.Client{Timeout: 10 * time.Second}
 	GetCatFact()
+	GetRandomUser()
 }
 
 
